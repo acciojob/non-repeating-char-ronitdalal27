5458fd95d7509ae983714ee6c57c1ee74aa1c4b1
ronitdalal27 <dalalronit131@gmail.com>
2025-01-09 12:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(s) {
+	// Create an object to store character counts
+    const charCount = {};
+
+    // Count the occurrence of each character
+    for (const char of s) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+
+    // Find the first non-repeated character
+    for (const char of s) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    // Return null if no non-repeated character is found
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
